9e4c55c8e010710e46402f1d522466b3750812fa
Edilson Lima <edilsonlm217@hotmail.com>
2020-11-27 13:22:15 -0400

    #1 Criação do arquivo de configuração para conexão com o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'bdhost0042.servidorwebfacil.com',
+  port: 3306,
+  username: 'superbin_root',
+  password: 'F@lcon2931',
+  database: 'superbin_superbingao',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
