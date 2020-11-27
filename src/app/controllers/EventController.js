f880ddd84fa699ef62166148d60729e93f59b4c3
Edilson Lima <edilsonlm217@hotmail.com>
2020-11-27 13:15:30 -0400

    #1 Implementação do método store e show

---
+++ src/app/controllers/EventController.js
@@ -0,0 +1,50 @@
+import Event from '../models/Event';
+import Validated from '../models/Validated';
+
+class EventController {
+  async store(req, res) {
+    const {
+      edition,
+      name,
+      initial_lot,
+      initial_card,
+      event_beginning,
+      card_price
+    } = req.body;
+
+    const event = await Event.create({
+      edition,
+      name,
+      initial_lot,
+      initial_card,
+      event_beginning,
+      is_active: true,
+      card_price,
+    });
+
+    return res.json(event);
+  }
+
+  async show(req, res) {
+    const { event_id } = req.query;
+
+    const event = await Event.findByPk(event_id);
+
+    if (!event) {
+      return res.status(400).json({ message: 'No events found' });
+    }
+
+    const selled_amount = await Validated.count({
+      where: {
+        edition: event.edition,
+      },
+    });
+
+    return res.json({
+      ...event.dataValues,
+      selled_amount: selled_amount,
+    });
+  }
+}
+
+export default new EventController();
