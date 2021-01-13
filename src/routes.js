9f278f149d976811f18c4e6fd3d98ff524a6aa40
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-13 14:35:56 -0400

    Criação de rota para exposição do método WinnerController

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import EventController from './app/controllers/EventController';
 import OrderController from './app/controllers/OrderController';
+import WinnerController from './app/controllers/WinnerController';
 import SearchController from './app/controllers/SearchController';
 import SessionController from './app/controllers/SessionController';
 import ValidatedCardController from './app/controllers/ValidatedCardController';
@@ -12,10 +13,12 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/session', SessionController.store);
+routes.post('/winner', WinnerController.show);
 
 routes.use(authMiddleware);
 routes.post('/users', UserController.store);
 
+routes.get('/events/all', EventController.index);
 routes.post('/event', EventController.store);
 routes.get('/events', EventController.show);
 
