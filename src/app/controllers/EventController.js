82b8fc5b0e4b737891a32adbb7357ebc4816cf65
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-22 15:06:06 -0400

    Implementação de try catch

--- src/app/controllers/EventController.js
+++ src/app/controllers/EventController.js
@@ -38,58 +38,63 @@ class EventController {
   }
 
   async show(req, res) {
-    const { event_id } = req.query;
+    try {
+      const { event_id } = req.query;
 
-    const event = await Event.findByPk(event_id);
+      const event = await Event.findByPk(event_id);
 
-    if (!event) {
-      return res.status(400).json({ message: 'No events found' });
+      if (!event) {
+        return res.status(400).json({ message: 'No events found' });
+      }
+
+      const selled_amount = await Validated.count({
+        where: {
+          edition: event.edition,
+        },
+      });
+
+      const chave_aplicacao = 'bac71631-6746-4fec-a391-79426b0568d5';
+      const chave_api = 'fdef8cea32652c3484c7';
+
+      const response = await axios.get(`https://api.awsli.com.br/v1/produto?chave_aplicacao=${chave_aplicacao}&chave_api=${chave_api}`);
+
+      // Implementação para recuperação do produc_id e product_status
+      var product_id = null;
+      var product_status = null;
+      response.data.objects.map(product => {
+        if (product.sku === event.edition) {
+          product_id = product.id;
+          product_status = product.ativo;
+        }
+      });
+
+      // Implementação para recuperação da primeira e da última cartela vendida
+      const first_card = await Validated.findOne({
+        where: {
+          edition: event.edition,
+        },
+        order: [['createdAt', 'ASC']],
+      });
+
+      const last_card = await Validated.findOne({
+        where: {
+          edition: event.edition,
+        },
+        order: [['createdAt', 'DESC']],
+      });
+
+      return res.json({
+        ...event.dataValues,
+        selled_amount: selled_amount,
+        product_id,
+        product_status,
+        first_selled_card: first_card.dataValues.codigo,
+        last_selled_card: last_card.dataValues.codigo,
+      });
+    } catch {
+      return res.status(500).json({ message: 'unable to connect with lojinha paroquial' });
     }
 
-    const selled_amount = await Validated.count({
-      where: {
-        edition: event.edition,
-      },
-    });
-
-    const chave_aplicacao = 'bac71631-6746-4fec-a391-79426b0568d5';
-    const chave_api = 'fdef8cea32652c3484c7';
-
-    const response = await axios.get(`https://api.awsli.com.br/v1/produto?chave_aplicacao=${chave_aplicacao}&chave_api=${chave_api}`);
-
-    // Implementação para recuperação do produc_id e product_status
-    var product_id = null;
-    var product_status = null;
-    response.data.objects.map(product => {
-      if (product.sku === event.edition) {
-        product_id = product.id;
-        product_status = product.ativo;
-      }
-    });
-
-    // Implementação para recuperação da primeira e da última cartela vendida
-    const first_card = await Validated.findOne({
-      where: {
-        edition: event.edition,
-      },
-      order: [['createdAt', 'ASC']],
-    });
-
-    const last_card = await Validated.findOne({
-      where: {
-        edition: event.edition,
-      },
-      order: [['createdAt', 'DESC']],
-    });
-
-    return res.json({
-      ...event.dataValues,
-      selled_amount: selled_amount,
-      product_id,
-      product_status,
-      first_selled_card: first_card.dataValues.codigo,
-      last_selled_card: last_card.dataValues.codigo,
-    });
   }
 }
 
